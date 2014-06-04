cb071b9457d9f874346c0a6576dd2d53f6c85fd2
Tim Cameron Ryan <id@timryan.org>
2014-06-03 17:09:06 -0700

    Welcome to marktype

--- index.js
+++ index.js
@@ -10,109 +10,50 @@ function escapify (str)
   })
 }
 
-function generate (data)
+function generate (line)
 {
-  var lines = data.split(/\n/);
-  var out = [];
-
-  for (var i = 0; i < lines.length; i++) {
-    var line = lines[i];
-
-    // Skip empty lines
-    if (line.match(/^\s*$/)) {
-      continue;
-    }
-
-    // Eat multi-line comments
-    if (line.match(/^\s*\/\*/)) {
-      while (lines[i + 1] != null && !lines[i + 1].match(/^\s*\*\//)) {
-        i++;
-      }
-      i++;
-      continue;
-    }
-
-    // Headers
-    if (line.match(/^#/)) {
-      out.push('##' + line);
-      out.push('');
-
-      // Immediately subsequent para
-      while (lines[++i] != null && lines[i].match(/^\s*$/)) {
-        continue;
-      }
-      out.push(lines[i]);
-      out.push('');
-    }
-
-    // Code sections (following signatures).
-    else if (line.match(/^```/)) {
-      out.push(line);
-      while (!(line = lines[++i]).match(/^```/)) {
-        out.push(line);
-      }
-      out.push(line + '\n\n');
-    }
-
-    // List items or space-indented paras.
-    else if (line.match(/^\*\s*(.*)|^\s+/)) {
-      var str = line.replace(/^\s*|\s*$/g, '');
-      if (lines[i + 1].match(/^\s*$/)) {
-        str += '\n';
-        ++i;
-      }
-      out.push(str);
-    }
-
-    // Signatures.
-    else {
-      var sig = line
-        // replace `
-        .replace(/`/g, '')
-        // type / return type (ie: [string] process.platform)
-        .replace(/^([\w<>]+)(?=\s+\w)/i, function (str) {
-          return '<i>' + str.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</i>&nbsp;'
+  var sig = line
+    // replace `
+    .replace(/`/g, '')
+    // type / return type (ie: [string] process.platform)
+    .replace(/^([\w<>]+)(?=\s+\w)/i, function (str) {
+      return '<i>' + str.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</i>&nbsp;'
+    })
+    // object property (ie: string process.[platform])
+    .replace(/(\.\w+|\w+\b(?!\s*\.))(?=\s*[\(\[\{=]|\s*$)/i, '<b>$1</b>')
+    // function arguments
+    .replace(/\(.*\)/g, function (str) {
+      return str
+        // paren spacing
+        .replace(/\(\s*/, '( ').replace(/\s*\)$/, ' )')
+        .replace(/\(\s+\)/, '()')
+        // codify arguments
+        .replace(/\b(([^,\(\)\[\]\{\}\|]+)\s*(\([^\(\)\[\]\{\}\|\s]*\))?\b)/g, function (_, arg) {
+          var split = arg.split(/\s+/);
+          if (split.length > 1) {
+            return '<i>' + escapify(split[0]) + '</i>&nbsp; ' + escapify(split.slice(1).join(' '));
+          } else {
+            return arg;
+          }
         })
-        // object property (ie: string process.[platform])
-        .replace(/(\.\w+|\w+\b(?!\s*\.))(?=\s*[\(\[\{=]|\s*$)/i, '<b>$1</b>')
-        // function arguments
-        .replace(/\(.*\)/g, function (str) {
-          return str
-            // paren spacing
-            .replace(/\(\s*/, '( ').replace(/\s*\)$/, ' )')
-            .replace(/\(\s+\)/, '()')
-            // codify arguments
-            .replace(/\b(([^,\(\)\[\]\{\}\|]+)\s*(\([^\(\)\[\]\{\}\|\s]*\))?\b)/g, function (_, arg) {
-              var split = arg.split(/\s+/);
-              if (split.length > 1) {
-                return '<i>' + escapify(split[0]) + '</i>&nbsp; ' + escapify(split.slice(1).join(' '));
-              } else {
-                return arg;
-              }
-            })
-            // italicize "or"
-            .replace(/\b\s+or\s+\b/g, ' *or* ')
-        })
-        // HTTP methods (ie: [POST] /:username/tweets)
-        .replace(/^([A-Z]+)\b/, '<b>$1</b>')
-        // HTTP path segments (ie: POST /[:username]/tweets)
-        .replace(/:(\w+)/, '`:$1`')
-        // func -> return
-        .replace(/->\s+([^>]+)$/, '&rarr; <i>$1</i>')
+        // italicize "or"
+        .replace(/\b\s+or\s+\b/g, ' *or* ')
+    })
+    // HTTP methods (ie: [POST] /:username/tweets)
+    .replace(/^([A-Z]+)\b/, '<b>$1</b>')
+    // HTTP path segments (ie: POST /[:username]/tweets)
+    .replace(/:(\w+)/, '`:$1`')
+    // func -> return
+    .replace(/->\s+([^>]+)$/, '&rarr; <i>$1</i>')
 
-      if (sig.match(/\{\s*$/)) {
-        while (lines[i + 1] != null && !lines[i + 1].match(/^\s*\}/)) {
-          sig += '  \n&nbsp; ' + lines[++i].replace(/(\S.*\S)/, '`$1`').replace(/^\s+/g, '&nbsp;&nbsp;');
-        }
-        sig += '  \n&nbsp; ' + lines[++i];
-      }
-
-      out.push('&#x20;<a href="#api-' + safeify(line) + '" name="api-' + safeify(line) + '">#</a> ' + sig + '  ');
-      out.push(lines[++i] + '\n');
+  if (sig.match(/\{\s*$/)) {
+    while (lines[i + 1] != null && !lines[i + 1].match(/^\s*\}/)) {
+      sig += '  \n&nbsp; ' + lines[++i].replace(/(\S.*\S)/, '`$1`').replace(/^\s+/g, '&nbsp;&nbsp;');
     }
+    sig += '  \n&nbsp; ' + lines[++i];
   }
 
-  return out.join('\n');
+  return ('&#x20;<a href="#api-' + safeify(line) + '" name="api-' + safeify(line) + '">#</a> ' + sig + '  ');
 }
 
 function restore (data)
@@ -122,7 +63,7 @@ function restore (data)
       return line
         .replace(/&#x20;/g, ' ')
         .replace(/<[^>]+>/g, '')
-        .replace(/^ # /g, '')
+        .replace(/^ # /g, '[#] ')
         .replace(/&nbsp;/g, '')
         .replace(/&lt;/g, '<')
         .replace(/&gt;/g, '>')
@@ -136,5 +77,5 @@ function restore (data)
 }
 
 
-exports.generate = generate;
+exports.convert = generate;
 exports.restore = restore;
--- markdocs.js
+++ markdocs.js
@@ -1,137 +1,35 @@
 #!/usr/bin/env node
 
+var marktype = require('./index.js');
+
+if (process.argv.length < 4) {
+  console.error('Usage: marktype (convert|restore) file.md');
+  process.exit(1);
+}
+
+
+var lines = [];
+
 var fs = require('fs');
-var marked = require('marked');
-var markdocs = require('./index');
-
-// markdocs preview file.txt [-b]
-// markdocs generate file.txt
-// markdocs clear [README.md]
-
-var parser = require('nomnom')
-
-parser
-  .script('markdocs')
-  .command('preview')
-  .option('input', {
-    position: 1,
-    full: 'file.txt',
-    required: true,
-  })
-  .option('browser', {
-    abbr: 'b',
-    flag: true,
-    help: 'preview immediately in browser.'
-  })
-  .callback(preview)
-  .help('preview compiled api markdown')
-
-parser
-  .script('markdocs')
-  .command('generate')
-  .option('target', {
-    position: 1,
-    full: 'target.md',
-    default: 'README.md',
-    help: 'markdown file to regenerate'
-  })
-  .callback(generate)
-  .help('insert api docs into an existing markdown file. inbetween <!--markdocs-->...<!--/markdocs--> comments')
-
-parser
-  .script('markdocs')
-  .command('restore')
-  .option('target', {
-    position: 1,
-    full: 'target.md',
-    default: 'README.md',
-    help: 'restores text representation.'
-  })
-  .callback(restore)
-  .help('restores text version of markdown file. inbetween <!--markdocs-->...<!--/markdocs--> comments')
-
-parser.parse()
-
-
-/**
- * api
- */
-
-function mungefile (file, callback)
-{
-  fs.readFile(file, 'utf-8', function (err, data) {
-    callback(err, data, function (outfile, data, next) {
-      fs.writeFile(outfile || file, data, next);
-    })
-  })
-}
-
-function exit (err) {
-  throw err
-}
-
-function restoretags (data, cb) {
-  return data.replace(/<!--markdocs:generated-->\s*([\S\s]*?)<!--\/markdocs:generated-->/g, function (str, source) {
-    return '<!--markdocs-->\n\n'
-      + markdocs.restore(source) + '\n\n'
-      + '<!--/markdocs-->';
-  });
-}
-
-function generatetags (data, cb) {
-  return data.replace(/<!--markdocs-->\s*([\S\s]*?)<!--\/markdocs-->/g, function cb (str, source) {
-    return '<!--markdocs:generated-->\n\n'
-      + markdocs.generate(source) + '\n'
-      + '<!--/markdocs:generated-->';
-  });
-}
-
-
-/**
- * entry
- */
-
-function preview (opts)
-{
-  console.log('previewing:', opts.input);
-  mungefile(opts.input, function (err, data, write) {
-
-    fs.writeFileSync('/tmp/markdocs.html', '<style>body { font: 14px Helvetica, arial, freesans, clean, sans-serif; width: 750px; padding: 20px 30px; line-height: 1.5; }</style>' + marked(markdocs.generate(data)));
-    console.log('open: file:///tmp/markdocs.html')
-    if (opts.browser) {
-      require('open')('file:///tmp/markdocs.html')
+var filename = process.argv[3];
+require('readline').createInterface({
+  input: fs.createReadStream(filename),
+  terminal: false
+}).on('line', function(line){
+  if (process.argv[2] == 'restore') {
+    if (line.match(/^&#x20;/)) {
+      lines.push(marktype.restore(line));
+    } else {
+      lines.push(line);
     }
-  })
-}
-
-function generate (opts)
-{
-  console.log('into:', opts.target)
-  mungefile(opts.target, function (err, data, write) {
-    var transformed = generatetags(data);
-    if (transformed == data) {
-      console.error('ERROR: No <!--markdocs--> tags found. Aborting.');
-      process.exit(1);
+  } else {
+    if (line.match(/^\[#\]/)) {
+      lines.push(marktype.convert(line.replace(/^\[#\]\s*/, '')));
+    } else {
+      lines.push(line);
     }
-    write(null, transformed, function (err) {
-      err && exit(err)
-      console.error('done.');
-    });
-  });
-}
-
-function restore (opts)
-{
-  console.log('restoring:', opts.target)
-  mungefile(opts.target, function (err, data, write) {
-    var transformed = restoretags(data);
-    if (transformed == data) {
-      console.error('ERROR: No <!--markdocs:generated--> tags found. Aborting.');
-      process.exit(1);
-    }
-    write(null, transformed, function (err) {
-      err && exit(err)
-      console.error('done.');
-    });
-  });
-}
+  }
+}).on('close', function () {
+  fs.writeFileSync(filename, lines.join('\n'), 'utf-8');
+  console.error('wrote to', filename);
+});
