a4991527198d64bbb0eca445dbe81d6fd695e60e
Tim Cameron Ryan <id@timryan.org>
2014-06-09 19:42:30 -0700

    Fixes truncation of the last line.

--- marktype.js
+++ marktype.js
@@ -12,24 +12,15 @@ var lines = [];
 
 var fs = require('fs');
 var filename = process.argv[3];
-require('readline').createInterface({
-  input: fs.createReadStream(filename),
-  terminal: false
-}).on('line', function(line){
-  if (process.argv[2] == 'restore') {
-    if (line.match(/^&#x20;/)) {
-      lines.push(marktype.restore(line));
-    } else {
-      lines.push(line);
-    }
-  } else {
-    if (line.match(/^\[#\]/)) {
-      lines.push(marktype.convert(line.replace(/^\[#\]\s*/, '')));
-    } else {
-      lines.push(line);
-    }
+
+var input = fs.readFileSync(filename, 'utf-8');
+
+fs.writeFileSync(filename, input.split(/\r?\n/).map(function (line) {
+  if (process.argv[2] == 'restore' && line.match(/^&#x20;/)) {
+    return marktype.restore(line);
+  } else if (line.match(/^\[#\]/)) {
+    return marktype.convert(line.replace(/^\[#\]\s*/, ''));
   }
-}).on('close', function () {
-  fs.writeFileSync(filename, lines.join('\n'), 'utf-8');
-  console.error('wrote to', filename);
-});
+  return line;
+}).join('\n'), 'utf-8');
+console.error('wrote to', filename);
